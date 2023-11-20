7c8750ef904613897b032474c7907e0f60d4833c
CMIT <CMIT@DESKTOP-SOP4B3I>
2023-11-20 17:39:05 +0530

    Add project files.

---
+++ BoolToVisibilityConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace VisibleConverterEx
+{
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //throw new NotImplementedException();
+            bool IsMouseOver;
+            bool.TryParse(value.ToString(), out IsMouseOver);
+            if(IsMouseOver)
+            {
+                return Visibility.Visible;
+            }
+            return Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //throw new NotImplementedException();
+            return new object();
+        }
+    }
+}
